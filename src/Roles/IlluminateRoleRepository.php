a88ca2d267409a8f0149e54e51b3192187d8a533
Joona Pesonen <joona.pesonen@videovisit.fi>
2025-03-12 10:35:09 +0200

    feat: compatibility for PHP 8.4
    - Addresses compatibility issues with PHP 8.4's deprecation notices

--- src/Roles/IlluminateRoleRepository.php
+++ src/Roles/IlluminateRoleRepository.php
@@ -40,7 +40,7 @@ class IlluminateRoleRepository implements RoleRepositoryInterface
      *
      * @return void
      */
-    public function __construct(string $model = null)
+    public function __construct(?string $model = null)
     {
         $this->model = $model;
     }
